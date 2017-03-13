09580b47011893fb6baaa446b1191e20b2763f39
Scott Zabrowski <scottzab@umich.edu>
2017-03-12 21:36:19 -0400

    9:36pm

--- SI206W17_project2.py
+++ SI206W17_project2.py
@@ -59,7 +59,7 @@ def find_urls(user_input):
 	#reg = r'http?:\/\/[a-zA-Z]{2}(?:\.[a-zA-Z0-9]{2}.)+?''
 	reg = r'\bhttp://\b\w+\.\S{2}[^\s]+|\bhttps://\b\w+\.\S{2}[^\s]+'
 	list_urls = re.findall(reg, user_input)
-	print (list_urls)
+	#print (list_urls)
 	return list_urls
 
 
@@ -98,7 +98,11 @@ lst_pages = []
 umsi_titles = {}
 names = []
 job = []
+experiment = []
 
+
+lst1 = []
+lst2 = []
 for x in range(0,12):
 	url = "https://www.si.umich.edu/directory?field_person_firstname_value=&field_person_lastname_value=&rid=All&page=" + str(x)
 	response = requests.get(url, headers = {'User-Agent': 'SI_CLASS'})
@@ -108,13 +112,11 @@ for x in range(0,12):
 	lst_pages.append(soup1)
 	names.append(people)
 	h = soup1.find_all(property = 'dc:title')
-	lst1 = []
-	lst2 = []
 	for c in h:
 		v = c.find('h2').text
 		lst1.append(v)
-	people = soup1.find_all("div",{"class":"views-row"})
-	for er in people:
+	people1 = soup1.find_all("div",{"class":"views-row"})
+	for er in people1:
 		titl = er.find_all(class_="field field-name-field-person-titles field-type-text field-label-hidden")
 		for grade in titl:
 			col = grade.find(class_="field-item even").text
@@ -128,7 +130,21 @@ for x in range(0,12):
 		#print (m)
 		umsi_titles[x] = m
 
+# print (umsi_titles)
+# print (lst1)
+# print (lst2)
+# for m in lst1:
+# 	v = lst1.index(m)
+# 	b = list_of_positions[v]
+# 	umsi_titles1[m] = b
 
+#mm = [' ', ' ', ' ', ' ', ' ', 'Academic Advisor', 'Academic Advisor', 'Academic Programs Coordinator', 'Accountant Senior', 'Adjunct Associate Professor of Information, School of Information', 'Adjunct Clinical Associate Professor of Information and iDream Program Manager, School of Information', 'Adjunct Professor of Business Economics and Public Policy, Stephen M Ross School of Business, Research Professor, Survey Research Center and ISR Center Director, Institute for Social Research', 'Administrative Assistant', 'Administrative Assistant Inter', 'Administrative Assistant Senior', 'Administrative Assistant and Events Coordinator', 'Administrative Director', 'Admissions Associate Dir Unit', 'Admissions and Student Affairs Assistant', 'Arthur F Thurnau Professor, Professor of Climate and Space Sciences and Engineering, College of Engineering and Professor of Information, School of Information', 'Arthur F Thurnau Professor, Professor of Education, School of Education and Professor of Information, School of Information', 'Arthur F Thurnau Professor, Professor of Electrical Engineering and Computer Science, College of Engr, Professor of Education, School of Education and Professor of Information, School of Information', 'Arthur F Thurnau Professor, Vice Provost for Academic Innovation, Office of the Provost and Executive Vice President for Academic Affairs, Dean of Libraries, University Library, Professor of Information, School of Information and Faculty Associate, Resear', 'Assistant Dean for Diversity, Equity, and Inclusion, School of Information', 'Assistant Director of Human Resources and Support Services', 'Assistant Director of Recruiting and Admissions, School of Information and Intermittent Lecturer in Social Work, School of Social Work', 'Assistant Director, Health Informatics Program', 'Assistant Professor of Art and Design, Penny W Stamps School of Art and Design and Assistant Professor of Information, School of Information', 'Assistant Professor of Art and Design, Penny W Stamps School of Art and Design and Assistant Professor of Information, School of Information', 'Assistant Professor of Electrical Engineering and Computer Science, College of Engineering and Assistant Professor of Information, School of Information', 'Assistant Professor of Family Medicine, Medical School and Assistant Professor of Information, School of Information', 'Assistant Professor of Information, School of Information', 'Assistant Professor of Information, School of Information', 'Assistant Professor of Information, School of Information', 'Assistant Professor of Information, School of Information', 'Assistant Professor of Information, School of Information', 'Assistant Professor of Information, School of Information and Assistant Professor of Art and Design. Penny W Stamps School of Art and Design', 'Assistant Professor of Information, School of Information and Assistant Professor of Electrical Engineering and Computer Science, College of Engineering', 'Assistant Professor of Information, School of Information and Assistant Professor of Electrical Engineering and Computer Science, College of Engineering', 'Assistant Professor of Information, School of Information and Assistant Professor of Electrical Engineering and Computer Science, College of Engineering', 'Assistant Professor of Information, School of Information and Assistant Professor of Electrical Engineering and Computer Science, College of Engineering', 'Assistant Professor of Information, School of Information and Assistant Professor of Electrical Engineering and Computer Science, College of Engineering', 'Assistant Professor of Information, School of Information and Assistant Professor of Electrical Engineering and Computer Science, College of Engineering', 'Assistant Professor of Information, School of Information and Assistant Professor of Health Behavior and Health Education, School of Public Health', 'Assistant Professor of Information, School of Information, Assistant Professor of Electrical Engineering and Computer Science, College of Engineering and Assistant Professor of Complex Systems, College of Literature, Science, and the Arts', 'Assistant Professor of Learning Health Sciences, Medical School and Assistant Professor of Information, School of Information', 'Assistant Professor of Social Work, School of Social Work and Assistant Professor of Information, School of Information', 'Associate Archivist and Assistant Director, Bentley Historical Library and Adjunct Lecturer in Information, School of Information', 'Associate Archivist, Bentley Historical Library and Assistant Director, Bentley Historical Library and Adjunct Lecturer in Information, School of Information', 'Associate Director UMSI Computing', 'Associate Director of Development & Alumni Relations', 'Associate Librarian, University of Michigan Library', 'Associate Professor Emerita of Information, School of Information', 'Associate Professor Emeritus of Information, School of Information', 'Associate Professor of  Pediatrics and Communicable Diseases, Medical School and Clinical Associate Professor of Information, School of Information', 'Associate Professor of Electrical Engineering and Computer Science, College of Engineering and Associate Professor of Information, School of Information', 'Associate Professor of Information, School of Information', 'Associate Professor of Information, School of Information', 'Associate Professor of Information, School of Information', 'Associate Professor of Information, School of Information', 'Associate Professor of Information, School of Information and Associate Professor of Economics, College of Literature, Science, and the Arts', 'Associate Professor of Information, School of Information and Associate Professor of Electrical Engineering and Computer Science, College of Engineering', 'Associate Professor of Information, School of Information and Associate Professor of Electrical Engineering and Computer Science, College of Engineering', 'Associate Professor of Information, School of Information and Associate Professor of Electrical Engineering and Computer Science, College of Engineering', 'Associate Professor of Information, School of Information and Associate Professor of Health Behavior and Health Education, School of Public Health', 'Associate Professor of Information, School of Information and Associate Professor of Health Management and Policy, School of Public Health', 'Associate Professor of Music, School of Music, Theatre & Dance and Associate Professor of Information, School of Information', 'Business Systems Analyst Assoc', 'Career Development & Engaged Learning Coordinator', 'Career Services Counselor and Internship Coordinator and Adjunct Lecturer in Information, School of Information', 'Chief Information Officer and Clinical Assistant Professor of Information, School of Information', 'Citizen Experience Design Community Development Liaison and Adjunct Lecturer in Information, School of Information', 'Clinical Associate Professor of  Information, School of Information', 'Clinical Associate Professor of Information, School of Information', 'Clinical Associate Professor of Information, School of Information', 'Communications Specialist', 'Community Engagement & Exchange Coordinator', 'Dean Emeritus, Arthur W Burks Collegiate Professor Emeritus of Information and Computer Science, Professor Emeritus of Information, School of Information, Professor Emeritus of Economics, College of Literature, Science, and the Arts and Professor Emeritus', 'Dean and Professor of Information, School of Information', 'Director Admissions and Student Affairs', 'Director of Career Development', 'Director of Development and Alumni Relations', 'Director of Development, Bentley Historical Library', 'Director of Engaged Learning Programs', 'Director of Finance', 'Director of Human Resources and Support Services', 'Director of Marketing and Communications', 'Director of Research Administration', 'Ehrenberg Director of Entrepreneurship, Adjunct Clinical Associate Professor of Information and Research Investigator, School of Information', 'Employer Relations Coordinator', 'Engagement Manager', 'Events Manager', 'Facilities Coordinator', 'Financial Specialist Senior', 'George Herbert Mead Collegiate Professor of Human-Computer Interaction, Professor of Information, School of Information and Professor of Electrical Engineering and Computer Science, College of Engineering', 'Graduate Programs Coordinator', 'Graphic Designer', 'HR Generalist Intermediate', 'Harold T Shapiro Collegiate Professor of Public Policy, Arthur F Thurnau Professor, Interim Provost and Executive Vice President for Academic Affairs, Office of the Provost  and Executive Vice President for Academic Affairs, Presidential Bicentennial Prof', 'Intermittent Lecturer I in Information, School of Information', 'Intermittent Lecturer in Information, School of Information', 'Intermittent Lecturer in Information, School of Information', 'Intermittent Lecturer in Information, School of Information', 'John F Nickoll Professor of Law, Professor of Law, Law School and Professor of Information, School of Information', 'Josiah Macy, Jr Professor of Medical Education, Chair, Department of Learning Health Sciences, Professor of Learning Health Sciences, Medical School, Professor of Information, School of Information and Professor of Health Management and Policy, School of ', 'Large-Scale Research Program Manager', 'Lead Developer, Digital Innovation Greenhouse and Adjunct Lecturer in Information, School of Information', 'Lecturer III in Information, School of Information', 'Lecturer III in Information, School of Information', 'Lecturer III in Information, School of Information and Intermittent Lecturer in Residential College, College of Literature, Science, and the Arts', 'Lecturer IV in Information and Research Investigator, School of Information', 'Lecturer IV in Information, School of Information', 'Librarian, Library Budget and Planning - Copyright, University Library', 'Librarian, Library Collection, University Library and Adjunct Lecturer in Information, School of Information', 'MHI Recruitment and Admissions Coordinator', 'Marketing Assistant Associate', 'Marketing Project Manager', 'Michael D Cohen Collegiate Professor of Information, Associate Dean for Research and Faculty Affairs, Professor of Information and Interim Director of Health Informatics, School of Information', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'PhD student', 'President Emeritus and University Professor of Science and Engineering', 'Professor Emerita of Information, School of Information', 'Professor Emerita of Information, School of Information', 'Professor Emerita of Information, School of Information, Professor Emerita of Computation and Information Systems, Stephen M Ross School of Business and Professor Emerita of Psychology, College of Literature, Science, and the Arts', 'Professor Emeritus of Information, School of Information', 'Professor Emeritus of Information, School of Information', 'Professor Emeritus of Information, School of Information and Professor Emeritus of Electrical Engineering and Computer Science, College of Engineering', 'Professor Emeritus of Information, School of Information and Professor Emeritus of Psychology, College of Literature, Science, and the Arts', 'Professor of Dentistry, Department of Periodontics and Oral Medicine, Associate Dean for Faculty Affairs and Institutional Effectiveness, School of Dentistry and Clinical Professor of Information, School of Information', 'Professor of Information, School of Information', 'Professor of Information, School of Information', 'Professor of Information, School of Information', 'Professor of Information, School of Information and Professor of History, College of Literature, Science, and the Arts', 'Professor of Information, School of Information and Professor of History, College of Literature, Science, and the Arts', 'Professor of Information, School of Information, Faculty Associate, Center for Political Studies, Institute for Social Research and Professor of Communication Studies, College of Literature, Science, and the Arts', 'Professor of Information, School of Information, Professor of Electrical Engineering and Computer Science, College of Engineering and Prof of Psychology, College of Literature, Science and the Arts', 'Professor of Information, School of Information, Professor of Electrical Engineering and Computer Science, College of Engineering and Professor of Linguistics, College of Literature, Science, and the Arts', 'Professor of Information, School of Information, Professor of Electrical Engineering and Computer Science, College of Engineering and Provost and Executive Vice President for Academic Affairs, Office of the Provost and Executive Vice President for Academi', 'Program Manager and Research Coordinator', 'Project Manager', 'Research Area Specialist , School of Information', 'Research Area Specialist Lead and Research Investigator, School of Information', 'Research Assistant Professor, School of Information', 'Research Fellow, Information, Research Investigator, Information and Lecturer III in Information, School of Information', 'Research Fellow, Information, School of Information', 'Research Fellow, Information, School of Information', 'Research Investigator, Information and Research Fellow, School of Information', 'Research Process Coordinator', 'Research Process Coordinator Senior', 'Research Process Manager', 'Research Professor, School of Information', 'Research and HR Administrative Assistant', 'Robert M Warner Collegiate Professor of Information, Professor of Information, School of Information and Faculty Associate, Institute for Social Research', 'School Registrar', 'School of Information Intern', 'Senior Assistant to the Dean', 'Senior Associate Dean for Academic Affairs and Professor of Information, School of Information', 'Senior Associate Librarian, Graduate Library, University Library and Intermittent Lecturer in Information, School of Information', 'Senior Associate Librarian, Learning and Teaching, University Library and Adjunct Lecturer in Curriculum Support, College of Literature, Science, and the Arts', 'Social Media Specialist', 'Solution Architect Lead and Adjunct Lecturer in Information, Sch of Information, Director of Infrastructure ActiveStep, App Programmer/Analyst Ld, Family Medicine, Medical School', 'Student Services Assistant', 'Systems Programmer Analyst', 'Systems Programmer/Analyst Lead', 'Undergraduate Program Manager', 'Unix and IT Security Administrator', 'Videographer', 'Visiting Associate Professor of Economics, College of Literature, Science, and the Arts and Adjunct Associate Professor of Information, School of Information', 'W K Kellogg Professor of Community Information, Associate Professor of Information, School of Information', 'Web Software Developer', 'William Warner Bishop Collegiate Professor of Information and Professor of Information, School of Information']
+
+
+# print (mm)
+#zz = (umsi_titles.values())
+#print (sorted(zz))
+#print (mm)
 	# print (lst1)
 	# print (lst2)
 
@@ -141,9 +157,9 @@ for x in range(0,12):
 ## RETURN VALUE: A list of strings: A list of just the text of 5 different tweets that result from the search.
 
 def get_five_tweets(input_string):
-	returnedList = []
+	returnlst = []
 	if input_string in CACHE_DICTION:
-		returnedList = CACHE_DICTION[input_string]
+		returnlst = CACHE_DICTION[input_string]
 	else:
 		search_results = api.search(input_string)
 		string = json.dumps(search_results)
@@ -154,12 +170,12 @@ def get_five_tweets(input_string):
 		for i in json_x["statuses"]:
 			final_dict[i['text']] = i['user']['created_at']
 		for k in final_dict:
-			returnedList.append(k)
-		CACHE_DICTION["twitter_University of Michigan"] = returnedList      
+			returnlst.append(k)
+		CACHE_DICTION["twitter_University of Michigan"] = returnlst      
 		f = open(CACHE_FNAME, 'w')
 		f.write(json.dumps(CACHE_DICTION))
 		f.close()
-	return returnedList[:5]
+	return returnlst[:5]
 
 
 ## PART 3 (b) - Write one line of code to invoke the get_five_tweets function with the phrase "University of Michigan" and save the result in a variable five_tweets.
@@ -178,7 +194,7 @@ for z in five_tweets:
 	one = find_urls(z)
 	for m in one:
 		tweet_urls_found.append(m)
-print(tweet_urls_found)
+#print(tweet_urls_found)
 
 
 ########### TESTS; DO NOT CHANGE ANY CODE BELOW THIS LINE! ###########
